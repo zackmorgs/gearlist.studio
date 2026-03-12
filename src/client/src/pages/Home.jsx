a5acdbffcb494452eeff692ca5c1986ecabf937c
zackmorgs <zackmorgenthaler@gmail.com>
2026-03-12 06:56:35 -0500

    frontpage dummy content

--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-let artist_example_list = [
+const artist_example_list = [
     {
         name: "James Hetfield",
         picture: "/assets/img/james-hetfield.webp"
@@ -17,20 +18,81 @@ let artist_example_list = [
         name: "Justin Chancellor",
         picture: "/assets/img/justin-chancellor.jpeg"
     }
-]
+];
+
+const instrument_example_list = [
+    {
+        name: "Fender Stratocaster",
+        picture: "/assets/img/stratocaster.webp"
+    },
+    {
+        name: "Gibson Les Paul",
+        picture: "/assets/img/les-paul.webp"
+    },
+    {
+        name: "Höfner 500/1 Ignition",
+        picture: "/assets/img/hofner-500-1-ignition-m.webp"
+    },
+    {
+        name: "Moog Sub 37",
+        picture: "/assets/img/moog-sub-37.webp"
+    }
+];
+
+const amp_example_list = [
+    {
+        name: "Marshall JVM410H",
+        picture: "/assets/img/marshall-jvm410h-m.webp"
+    },
+    {
+        name: "Vox AC30",
+        picture: "/assets/img/vox-ac30-guitar-combo-amp-m.webp"
+    },
+    {
+        name: "Fender Twin Reverb",
+        picture: "/assets/img/fender-65-twin-reverb-m.webp"
+    },
+    {
+        name: "Mesa Boogie Dual Rectifier",
+        picture: "/assets/img/mesa-boogie-dual-rectifier-100-watt-tube-head-m.webp"
+    }
+];
+
+const pedal_example_list = [
+    {
+        name: "Boss DS-1 Distortion",
+        picture: "/assets/img/boss-ds-1-distortion-m.webp"
+    },
+    {
+        name: "Pro Co RAT",
+        picture: "/assets/img/pro-co-rat-m.webp"
+    },
+    {
+        name: "Strymon blueSky Reverberator",
+        picture: "/assets/img/strymon-bluesky-reverberator-m.webp"
+    },
+    {
+        name: "Strymon TimeLine",
+        picture: "/assets/img/strymon-timeline-xl.webp"
+    }
+];
+
+
 
 export default function Home() {
     return (
         <>
-            <header className="panel bg-black">
+            <header className="panel">
                 <h1>Discover the Gear Behind the Sound</h1>
                 <p>This website aims to be the ultimate database of guitars, amps, pedals, plugins, and studio equipment used by musicians around the world.</p>
             </header>
 
-            <section id="cta_artists" className="panel bg-black text-center">
+            <section id="cta_artists" className="panel text-center">
                 <img src="/assets/svg/icon-artist.svg" alt="Artist Icon" className="section-icon" />
-
                 <h2>Artists</h2>
+                <Link to="/artists" className="btn">
+                    <span>Explore Artists</span> <img src="/assets/svg/icon-arrow-forward.svg" alt="Arrow Forward Icon" className="btn-icon" />
+                </Link>
                 <p>Explore the gear via our curated artist profiles.</p>
                 <ul className="artist-list">
                     {artist_example_list.map((artist, index) => (
@@ -44,26 +106,69 @@ export default function Home() {
                 </ul>
             </section>
 
-            <section id="cta_guitars" className="panel bg-black text-center">
-                <h2>Guitars & Basses</h2>
-                <p>See a database of guitars and basses that artists use.</p>
+            <section id="cta_instruments" className="panel text-center">
+                <img alt="Instrument Icon" className="section-icon" src="/assets/svg/icon-piano.svg"></img>
+                <h2>Instruments</h2>
+                <Link to="/instruments" className="btn"><span>Explore Instruments</span> <img src="/assets/svg/icon-arrow-forward.svg" alt="Arrow Forward Icon" className="btn-icon" /></Link>
+                <p>See a database of guitars, bass, keyboards and other instruments that artists use.</p>
+                <ul className="instrument-list">
+                    {instrument_example_list.map((instrument, index) => (
+                        <li key={index} className="instrument-card">
+                            <img className="instrument-photo" src={instrument
+                                .picture} alt={instrument.name} />
+                            <div className="overlay">
+                                <h3>{instrument.name}</h3>
+                            </div>
+                        </li>
+                    ))}
+
+                </ul>
             </section>
 
 
-            <section id="cta_amps" className="panel bg-black text-center">
+            <section id="cta_amps" className="panel text-center">
+                <img alt="Amp Icon" className="section-icon" src="/assets/svg/icon-amp.svg"></img>
                 <h2>Amps</h2>
-                <p>See a database of guitar and bass amps that artists use.</p>
+                <Link to="/amps" className="btn"><span>Explore Amps</span> <img src="/assets/svg/icon-arrow-forward.svg" alt="Arrow Forward Icon" className="btn-icon" /></Link>
+
+                <ul className="amp-list">
+                    {amp_example_list.map((amp, index) => (
+                        <li key={index} className="amp-card">
+                            <img className="amp-photo" src={amp.picture
+                            } alt={amp.name} />
+                            <div className="overlay">
+                                <h3>{amp.name}</h3>
+                            </div>
+                        </li>
+                    ))}
+
+                </ul>
             </section>
 
 
-            <section id="cta_pedals" className="panel bg-black text-center">
+            <section id="cta_pedals" className="panel text-center">
+                <img alt="Pedal Icon" className="section-icon" src="/assets/svg/icon-pedal.svg"></img>
                 <h2>Pedals</h2>
+                <Link to="/pedals" className="btn"><span>Explore Pedals</span> <img src="/assets/svg/icon-arrow-forward.svg" alt="Arrow Forward Icon" className="btn-icon" /></Link>
                 <p>See a database of guitar and bass pedals that artists use.</p>
+                <ul className="pedal-list">
+                    {pedal_example_list.map((pedal, index) => (
+                        <li key={index} className="pedal-card">
+                            <img className="pedal-photo" src={pedal.picture
+                            } alt={pedal.name} />
+                            <div className="overlay">
+                                <h3>{pedal.name}</h3>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </section>
 
-            <section id="cta_plugins" className="panel bg-black text-center">
+            <section id="cta_plugins" className="panel text-center">
+                <img alt="Plugin Icon" className="section-icon" src="/assets/svg/icon-puzzle.svg"></img>
                 <h2>Plugins</h2>
-                <p>See a database of plugins that artists use.</p>
+                <Link to="/plugins" className="btn">Explore Plugins</Link>
+                <p>See a database of VST/VSTi, CLAP, and RTAS plugins that artists use.</p>
             </section>
         </>
     );
