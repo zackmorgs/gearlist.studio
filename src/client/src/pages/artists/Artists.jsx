177e9445cffadf94487209be03ef2183cf124722
zackmorgs <zackmorgenthaler@gmail.com>
2026-03-16 06:47:27 -0500

    fix: adjust padding in body styles and update artist link button order

--- src/client/src/pages/artists/Artists.jsx
+++ src/client/src/pages/artists/Artists.jsx
@@ -128,12 +128,10 @@ export default function Artists() {
                         </ul>
                         <br />
                         <Link to="/artists/new" className="btn btn-primary">
-                            <span>Add Artist</span> <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg> <span>Add Artist</span>
                         </Link>
                     </>
                 )}
-
-
             </section>
         </>
     );
