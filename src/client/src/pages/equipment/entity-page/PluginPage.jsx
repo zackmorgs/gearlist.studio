592e9a4ca4e15896dae7fd31707ec6143383eb4f
zackmorgs <zackmorgenthaler@gmail.com>
2026-03-18 08:03:57 -0500

    feat: add SVG icons to edit links in artist, band, and equipment pages

--- src/client/src/pages/equipment/entity-page/PluginPage.jsx
+++ src/client/src/pages/equipment/entity-page/PluginPage.jsx
@@ -38,7 +38,7 @@ export default function PluginPage() {
                     <img src={plugin.imageUrl} alt={plugin.displayName} style={{ maxWidth: "300px", marginBottom: "1rem" }} />
                 )}
                 <h1>{plugin.displayName}</h1>
-                <Link to={`/equipment/plugins/${slug}/edit`} className="btn">Edit</Link>
+                <Link to={`/equipment/plugins/${slug}/edit`} className="btn"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M216-216h51l375-375-51-51-375 375v51Zm-72 72v-153l498-498q11-11 23.84-16 12.83-5 27-5 14.16 0 27.16 5t24 16l51 51q11 11 16 24t5 26.54q0 14.45-5.02 27.54T795-642L297-144H144Zm600-549-51-51 51 51Zm-127.95 76.95L591-642l51 51-25.95-25.05Z" /></svg> Edit</Link>
             </header>
             <section className="panel">
                 <h2>Description</h2>
