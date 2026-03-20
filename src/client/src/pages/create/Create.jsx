b6691fc9a3787b23ef5c46fbfd76ad529a2e9366
zackmorgs <zackmorgenthaler@gmail.com>
2026-03-20 07:34:16 -0500

    style: enhance layout of Create page for improved structure and readability

--- src/client/src/pages/create/Create.jsx
+++ src/client/src/pages/create/Create.jsx
@@ -4,37 +4,49 @@ import { Link, useParams } from "react-router-dom";
 export default function Create() {
     return (
         <>
-            <header className="panel text-center">
-                <h1>Create</h1>
+            <header>
+                <div className="container">
+                    <div className="panel text-center">
+                        <h1>Create</h1>
+                    </div>
+                </div>
             </header>
-            <section className="panel">
-                <p>You've elected to make a new item for our database. </p>
-                <p>Please select the appropriate category and fill out the item creation form.</p>
-            </section>
-            <section id="amps" className="panel">
-                <ul className="create-list">
-                    <li className="create-item">
-                        <Link to="/equipment/amps/new" className="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add an Amp</span>
-                        </Link>
-                    </li>
-                    <li className="create-item">
-                        <Link to="/equipment/cabs/new" className="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add a Cab</span>
-                        </Link>
-                    </li>
-                    <li className="create-item">
-                        <Link to="/equipment/pedals/new" className="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add a Pedal</span>
-                        </Link>
-                    </li>
-                    <li className="create-item">
-                        <Link to="/equipment/guitars/new" className="btn btn-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add a Guitar</span>
-                        </Link>
+            <section>
+                <div className="container">
+                    <div className="panel">
+                        <p>You've elected to make a new item for our database. </p>
+                        <p>Please select the appropriate category and fill out the item creation form.</p>
+                    </div>
+                </div>
+            </section >
+            <section id="amps">
+                <div className="container">
+                    <div className="panel">
+                        <ul className="create-list">
+                            <li className="create-item">
+                                <Link to="/equipment/amps/new" className="btn btn-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add an Amp</span>
+                                </Link>
+                            </li>
+                            <li className="create-item">
+                                <Link to="/equipment/cabs/new" className="btn btn-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add a Cab</span>
+                                </Link>
+                            </li>
+                            <li className="create-item">
+                                <Link to="/equipment/pedals/new" className="btn btn-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add a Pedal</span>
+                                </Link>
+                            </li>
+                            <li className="create-item">
+                                <Link to="/equipment/instruments/new" className="btn btn-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z" /></svg><span>Add an Instrument</span>
+                                </Link>
 
-                    </li>
-                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </section>
         </>
     );
